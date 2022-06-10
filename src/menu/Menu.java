4f0192e31e1aa48cf64b06096a3fc936c1f8167a
shivakumar.y <shivakumar.y@perfios.com>
2022-06-10 13:37:32 +0530

    Modified : modified menu class

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -3,53 +3,53 @@ package menu;
 import java.util.Scanner;
 
 public class Menu<T> {
-    public void linkedListOperations(){
-        System.out.println("\n Choose one operation: ");
-        System.out.println("1. list\n2. add\n3. remove\n4. search\n5. isEmpty\n6. size\n7. append\n8. index\n9. insert\n10.pop\n11. exit");
-        int counter = 0;
-
-        Scanner sc = new Scanner(System.in);
-        LinkedList<T> list = new LinkedList<>();
-        try{
-            while(counter != 11) {
-                System.out.println();
-                System.out.println("Select any operation : ");
-                int option = sc.nextInt();
-                if (option > 0 && option < 12) {
-                    switch (option) {
-                        case (1) -> System.out.println("list");
-                        case (2) -> {
-                            System.out.println("please enter what you want : ");
-                            String data = sc.next();
-                            list.add((T) data);
-//                            list.print();
-                        }
-                        case (3) -> {
-                            System.out.println("please enter where you want to delete : ");
-                            T data = (T) sc.next();
-                            list.remove(data);
-                        }
-                        case (4) -> list.print();
-                        case (5) -> System.out.println("isEmpty");
-                        case (6) -> System.out.println("size");
-                        case (7) -> System.out.println("append");
-                        case (8) -> System.out.println("index");
-                        case (9) -> System.out.println("insert");
-                        case (10) -> System.out.println("pop");
-                        case (11) -> {
-                            System.out.println("Good Bye!!");
-                            counter = option;
-                        }
-                    }
-                } else {
-                    System.out.println("please choose option between 1-11 only!");
-                }
-            }
-        }catch (Exception e){
-            System.out.println("please choose between 1-11 only.");
-            linkedListOperations();
-        }
-    }
+//    public void linkedListOperations(){
+//        System.out.println("\n Choose one operation: ");
+//        System.out.println("1. list\n2. add\n3. remove\n4. search\n5. isEmpty\n6. size\n7. append\n8. index\n9. insert\n10.pop\n11. exit");
+//        int counter = 0;
+//
+//        Scanner sc = new Scanner(System.in);
+//        LinkedList<T> list = new LinkedList<>();
+//        try{
+//            while(counter != 11) {
+//                System.out.println();
+//                System.out.println("Select any operation : ");
+//                int option = sc.nextInt();
+//                if (option > 0 && option < 12) {
+//                    switch (option) {
+//                        case (1) -> System.out.println("list");
+//                        case (2) -> {
+//                            System.out.println("please enter what you want : ");
+//                            String data = sc.next();
+//                            list.add((T) data);
+////                            list.print();
+//                        }
+//                        case (3) -> {
+//                            System.out.println("please enter where you want to delete : ");
+//                            T data = (T) sc.next();
+//                            list.remove(data);
+//                        }
+//                        case (4) -> list.print();
+//                        case (5) -> System.out.println("isEmpty");
+//                        case (6) -> System.out.println("size");
+//                        case (7) -> System.out.println("append");
+//                        case (8) -> System.out.println("index");
+//                        case (9) -> System.out.println("insert");
+//                        case (10) -> System.out.println("pop");
+//                        case (11) -> {
+//                            System.out.println("Good Bye!!");
+//                            counter = option;
+//                        }
+//                    }
+//                } else {
+//                    System.out.println("please choose option between 1-11 only!");
+//                }
+//            }
+//        }catch (Exception e){
+//            System.out.println("please choose between 1-11 only.");
+//            linkedListOperations();
+//        }
+//    }
 
     public void queueOperation() {
         System.out.println(" ");
